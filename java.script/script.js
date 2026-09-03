212792151872151aeee1d70b877563d36d5707b2
Neyro11 <robin.koepfli@gmail.com>
2026-09-03 15:19:29 +0200

    js

--- java.script/script.js
+++ java.script/script.js
@@ -1,1 +1,23 @@
-//
+// Kontakt Code JS
+const form = document.querySelector("form");
+const contactButton = document.querySelector("#contact-button");
+let contactName = document.querySelector("#form-text");
+let contactMail = document.querySelector("#form-email");
+let contactMessage = document.querySelector("#form-message");
+
+const thanksMessage = () => {
+  alert("thank you for contacting us");
+};
+
+contactButton.addEventListener("click", () => {
+  if (!contactName === "" && !contactMail === "" && !contactMessage === "") {
+    console.log(`Name: ${contactName}
+               E-Mail: ${contactMail}
+               Nachricht: ${contactMessage}`);
+    thanksMessage();
+  }
+});
+form.addEventListener("submit", function (e) {
+  // Verhindert das Standardverhalten (Neuladen der Seite)
+  e.preventDefault();
+});
